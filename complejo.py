b19892e0ce270b54d6d5073f7dd92d16c0a796ea
vanerod <mv.rodriguez1@uniandes.edu.co>
2019-03-28 12:39:45 +0000

    Creacion pruebas.py

---
+++ complejo.py
@@ -0,0 +1,15 @@
+import numpy as np
+class Complejo():
+    def __init__(self , real, imaginario):
+        self.imaginario
+        self.real
+        self.norma
+    def conjugado():
+        self.imaginario = -imaginario
+        self.real =
+        
+    def calcula_norma():
+        return np.sqrt(imaginario**2 + real**2)
+    def pow(n):
+        return (real+imaginario)**n
+        
